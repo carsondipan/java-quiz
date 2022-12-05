9316426867517852e309be47e4811dfb4d703009
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2022-12-05 12:24:59 -0500

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-addEventListener
+var score = 0
+var timeEl = document.querySelector(".time")
+
+
+
+function takeQuiz() {
+    document.getElementById("button").addEventListener("click", )
+}
+
+
+function displayTime() {
+    var secondsLeft = 60;
+    var label = "seconds";
+    if (secondsLeft === 1) {
+        label = "second";
+    }
+    timeEl.textContent = secondsLeft + " " + label + " until quiz ends.";
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        displayTime(timeLeft);
+        if (secondsLeft === 0) {
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
+}
